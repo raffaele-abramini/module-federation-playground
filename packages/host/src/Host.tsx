adae19979601a03ace3d00bc9d380c04d9da1b3f
rabramini <rabramini@twilio.com>
2025-06-30 09:45:58 +0100

    replace vite with RSPACK

--- packages/host/src/Host.tsx
+++ packages/host/src/Host.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 
-const RemoteComponent = React.lazy(() => import('remotez/RemoteComponent'));
+// const RemoteComponent = React.lazy(() => import('remotez/RemoteComponent'));
 
 export const Host = () => {
   return <div style={{background: "wheat", padding: "2em"}}>HOST app
-    <RemoteComponent />
+    {/*<RemoteComponent />*/}
   </div>
 }
