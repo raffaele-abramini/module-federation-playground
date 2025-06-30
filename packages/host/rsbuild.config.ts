60cc2cf52614d7a3340046714992a63aea7251bb
rabramini <rabramini@twilio.com>
2025-06-30 13:29:41 +0100

    module federation rspack

--- packages/host/rsbuild.config.ts
+++ packages/host/rsbuild.config.ts
@@ -1,9 +1,29 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginModuleFederation } from "@module-federation/rsbuild-plugin";
 
 export default defineConfig({
-  plugins: [pluginReact()],
-  server:{
+  plugins: [
+    pluginReact(),
+    pluginModuleFederation({
+      name: 'host',
+      remotes: {
+        remote: 'remote@http://localhost:5002/mf-manifest.json',
+      },
+
+      shared: {
+        react: {
+          singleton: true,
+          eager: true,
+        },
+        'react-dom': {
+          singleton: true,
+          eager: true,
+        },
+      }
+    }),
+  ],
+  server: {
     port: 5001
   }
 });
--- packages/remote/rsbuild.config.ts
+++ packages/remote/rsbuild.config.ts
@@ -1,9 +1,26 @@
 import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
+import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
-  plugins: [pluginReact()],
-  server:{
+  plugins: [
+    pluginReact(),
+    pluginModuleFederation({
+      name: 'remote',
+      exposes: {
+        './Remote': './src/Remote.tsx',
+      },
+      shared: {
+        react: {
+          singleton: true,
+        },
+        'react-dom': {
+          singleton: true,
+        },
+      }
+    }),
+  ],
+  server: {
     port: 5002
   }
 });
